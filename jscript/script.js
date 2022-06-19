c052d13e5d885e39d8c17f6c98c30bdab1facee2
Kraine07 <aundre.bushay@gmail.com>
2022-06-19 13:26:09 -0500

    added menu modal, other tweaks

--- jscript/script.js
+++ jscript/script.js
@@ -1,3 +1,22 @@
-// CURRENT DATE AND TIME
+
+
+// DISPLAY CURRENT DATE
 var now = new Date();
 document.getElementsByClassName('time')[0].innerHTML=now.toDateString();
+
+
+// MENU MODAL
+const menuButton = document.getElementsByClassName("menu-btn")[0];
+const modal = document.getElementsByClassName("menu-modal")[0];
+
+// DISPLAYS MENU MODAL WHEN MENU BUTTON IS CLICKED
+menuButton.onclick = function(){
+    modal.style.display = "block";
+}
+
+// CLICK OUTSIDE THE MENU MODAL TO CLOSE
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
